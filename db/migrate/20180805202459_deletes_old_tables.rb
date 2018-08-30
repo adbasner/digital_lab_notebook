df1c0d2739f3e39ade8f9bd484e84e9df92956e8
Andrew Basner <andrewbasner@gmail.com>
2018-08-29 19:50:35 -0500

    remove bad migrations

--- db/migrate/20180805202459_deletes_old_tables.rb
+++
@@ -1,6 +0,0 @@
-class DeletesOldTables < ActiveRecord::Migration[5.2]
-  def down
-    drop_table(classrooms)
-    drop_table(studuent_teacher_pairs)
-  end
-end
--- db/migrate/20180805202845_delete_old_tables.rb
+++
@@ -1,6 +0,0 @@
-class DeleteOldTables < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :classrooms
-    drop_table :studuent_teacher_pairs
-  end
-end
